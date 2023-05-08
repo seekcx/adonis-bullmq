db4d36a999702e80b41116d18a77978e34bd621c
abel <abel@seek.cx>
2023-05-08 18:02:52 +0800

    add command & release 1.0.3

--- commands/index.ts
+++ commands/index.ts
@@ -1,1 +1,5 @@
-export default ['adonis-bullmq/build/commands/QueueListen', 'adonis-bullmq/build/commands/MakeJob'];
+export default [
+	'adonis-bullmq/build/commands/QueueListen',
+	'adonis-bullmq/build/commands/MakeJob',
+	'adonis-bullmq/build/commands/QueueDashboard',
+];
